b7b10243f0a9c735f53931e0ea38dc9e61b7f7c8
Tiago Guimarães <tab.guimaraes@gmail.com>
2025-08-20 07:53:51 -0300

    ex02 OK

--- ex02.js
+++ ex02.js
@@ -1,6 +1,10 @@
-index = 200;
+let index = 200;
+const body = document.querySelector("body");
 
 do {
   console.log(index);
+  let p = document.createElement("p");
+  p.innerText = `${index}`;
+  body.appendChild(p);
   index--;
 } while (index >= 10);
