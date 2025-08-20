e6606725c3156a4b5fa9bb2e846202d19fc409b0
Tiago Guimarães <tab.guimaraes@gmail.com>
2025-08-20 08:36:09 -0300

    ex03 OK

--- ex05.js
+++ ex05.js
@@ -1,9 +1,18 @@
 let index = 0;
+const input = document.querySelector("#number");
+const section = document.querySelector("section");
+let entrada = 0;
 
-let entrada1 = prompt("Digite o valor: ");
+input.addEventListener("input", function (evento) {
+  entrada = +evento.target.value;
+  Multiplicar(entrada);
+});
 
-while (index <= 10) {
-  let resultado = entrada1 * index;
-  console.log(resultado);
-  index++;
+function Multiplicar(entrada) {
+  while (index <= 10) {
+    let p = document.createElement("p");
+    p.innerHTML = `${entrada} x ${index} = <span>${entrada * index}</span>`;
+    section.appendChild(p);
+    index++;
+  }
 }
